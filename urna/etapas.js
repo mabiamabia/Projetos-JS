defba61810bf6382650ecf9f80cf2f361aa37794
priscila.costa <priscila.costa@g.globo>
2023-01-03 20:37:09 -0300

    feat: add new js archieve

---
+++ urna/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        name: " Fulano de tal",
+        partido: "ABC",
+        fotos: [
+          { url: "./img/person-3.jpg", legenda: "Vereador", small: false },
+        ],
+      },
+      {
+        numero: 77222,
+        name: "Beltrano da Silva",
+        partido: "DEFG",
+        fotos: [{ url: "./img/person-4.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        name: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { url: "./img/person-5.jpeg", legenda: "Prefeito" },
+          { url: "./img/person-6.webp", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: 84,
+        name: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "./img/person-7.png", legenda: "Prefeito" },
+          { url: "./img/person-0.webp", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
