5e1cf403b87dfb226e44ec2c51f3586e6cf5bc99
priscila.costa <priscila.costa@g.globo>
2023-01-03 21:50:33 -0300

    feat: add logical changes

--- urna/script.js
+++ urna/script.js
@@ -1,9 +1,52 @@
-const msg1 = document.getElementById("msg1");
+let seuVotoPara = document.querySelector(".d-1-1 span");
+let cargo = document.querySelector(".d-1-2 span");
+let descricao = document.querySelector(".d-1-4");
+let aviso = document.querySelector(".d-2");
+let lateral = document.querySelector(".d-1 right");
+let numeros = document.querySelector(".d-1-3");
 
-msg1.addEventListener("click", () => {
-  document.body.style.background = randomBg();
-});
+let etapaAtual = 0;
+let numero = "";
 
-function confirmar() {
-  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+function comecaEtapa() {
+  let etapa = etapas[etapaAtual];
+
+  let numeroHtml = "";
+
+  for (let i = 0; i < etapa.numeros; i++) {
+    if (i === 0) {
+      numeroHtml += '<div class="numero pisca"></div>';
+    } else {
+      numeroHtml += '<div class="numero"></div>';
+    }
+  }
+
+  seuVotoPara.style.display = "none";
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = "";
+  aviso.style.display = "none";
+  lateral.innerHTML = "";
+  numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {}
+
+function clicou(n) {
+  let numero = document.querySelector(".numero.pisca");
+  if (numero !== null) {
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+  }
+}
+
+function branco() {
+  alert("Clicou em branco!");
+}
+
+function corrige() {
+  alert("Clicou em corrige!");
+}
+
+function confirma() {
+  alert("Clicou em confirma!");
 }
